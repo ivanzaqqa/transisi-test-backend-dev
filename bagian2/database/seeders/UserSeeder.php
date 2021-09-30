c1eb571e25b339edbcf1926784adbd723088aabe
Ivan Zaqqa <velocitysages@outlook.com>
2021-09-30 09:26:18 +0700

    make migrations and seeder for authentication

---
+++ bagian2/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@transisi.id',
+            'password' => bcrypt('transisi'),
+            'remember_token' => Str::random(60),
+        ]);
+    }
+}
